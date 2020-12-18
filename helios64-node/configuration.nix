00cfc5b34192c6c3b80d456391c5176f10ecbb66
Moritz Angermann <moritz.angermann@gmail.com>
2020-12-18 09:40:22 +0800

    Add back populateRootCommands

--- helios64-node/configuration.nix
+++ helios64-node/configuration.nix
@@ -5791,6 +5791,13 @@
       };
   };
 
+  sdImage = {
+    populateRootCommands = ''
+      mkdir -p ./files/boot
+      ${config.boot.loader.generic-extlinux-compatible.populateCmd} -c ${config.system.build.toplevel} -d ./files/boot
+    '';
+  };
+
   # the installation media is also the installation target,
   # so we don't want to provide the installation configuration.nix.
   installer.cloneConfig = false;
